4d6d51163c29d5782d574f63aadbad0c33760de3
KuldipUX <Kuldipsingh2311008@gmail.com>
2026-03-31 02:58:37 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     console.log(user);
     try {
       const res = await axios.post(
-        `http://localhost:8080/api/v1/user/login`,
+        https://backend-chattar.onrender.com/api/v1/user/login`,
         user,
         {
           headers: {
